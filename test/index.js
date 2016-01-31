fcb8a21b25ee8189bdf7e0d04b3507ac18c49dfa
Ryan Tsao <rtsao@uber.com>
2016-01-30 22:50:09 -0800

    added basic test with repo fixture

--- test/index.js
+++ test/index.js
@@ -1,6 +1,14 @@
 var test = require('tape');
+var path = require('path');
+var execSync = require('child_process').execSync;
 
-test('todo tests', function (t) {
-  t.pass('dummy');
+var FIXTURE_PATH = path.join(process.cwd(), 'test', 'fixtures', 'test-repo');
+var HASH = 'fe9f4ce';
+
+test('test commit hash of fixture ok', function (t) {
+  var res = execSync('git rev-parse --short HEAD', {
+    cwd: FIXTURE_PATH
+  });
+  t.equal(res.toString().trim(), HASH, 'hash matches');
   t.end();
 });
