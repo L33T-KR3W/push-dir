4fc09d32bdccdfc644e288d4fb5c51533d5ece74
-h <scott.the.hardy@gmail.com>
2016-06-17 23:01:08 -0700

    help tests

--- test/fixture/test-works-help.sh
+++ test/fixture/test-works-help.sh
@@ -3,31 +3,64 @@
 git remote add origin ../fixture-remote/.git
 
 
-node $PUSH_DIR --help
+HELP_MESSAGE=`node $PUSH_DIR --help`
 if [ $? -ne 0 ]; then
   exit 11
 fi
-
-
-node $PUSH_DIR -h
-if [ $? -ne 0 ]; then
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
   exit 12
 fi
 
 
-node $PUSH_DIR build:gh-pages --help
+HELP_MESSAGE=`node $PUSH_DIR -h`
 if [ $? -ne 0 ]; then
   exit 13
 fi
-
-
-node $PUSH_DIR origin build:gh-pages --help
-if [ $? -ne 0 ]; then
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
   exit 14
 fi
 
 
-HELP_MESSAGE=`node $PUSH_DIR --help`
-if [ ${#HELP_MESSAGE} -lt 100 ]; then
+HELP_MESSAGE=`node $PUSH_DIR build:gh-pages --help`
+if [ $? -ne 0 ]; then
   exit 15
+fi
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
+  exit 16
+fi
+
+
+HELP_MESSAGE=`node $PUSH_DIR origin build:gh-pages --help`
+if [ $? -ne 0 ]; then
+  exit 17
+fi
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
+  exit 18
+fi
+
+
+HELP_MESSAGE=`node $PUSH_DIR`
+if [ $? -ne 1 ]; then
+  exit 19
+fi
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
+  exit 20
+fi
+
+
+HELP_MESSAGE=`node $PUSH_DIR asdf`
+if [ $? -ne 1 ]; then
+  exit 21
+fi
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
+  exit 22
+fi
+
+
+HELP_MESSAGE=`node $PUSH_DIR asdf asdf asdf`
+if [ $? -ne 1 ]; then
+  exit 23
+fi
+if [ ${#HELP_MESSAGE} -lt 100 ]; then
+  exit 24
 fi
